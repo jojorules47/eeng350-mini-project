f1ac2afed60ae4b04ea1ae2730e7f77585a8f834
Grant <gredmond@mines.edu>
2022-03-13 19:48:15 -0600

    cv update

--- pi/Demo1CommsPy/Demo1CommsCV.py
+++ pi/Demo1CommsPy/Demo1CommsCV.py
@@ -5,6 +5,44 @@ import time
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
+import cv2 as cv
+from picamera import PiCamera
+from time import sleep
+import numpy as np
+import sys
+
+mtx = np.fromfile('mtx.npy').reshape(3,3)
+dist = np.fromfile('dist.npy')
+
+width = 512
+height = 288
+camera = PiCamera()
+camera.resolution = (width,height)
+camera.framerate = 30
+camera.iso = 100
+sleep(2)
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+g = camera.awb_gains 
+camera.awb_mode = 'off'
+camera.awb_gains = g
+
+lowColor = np.array([75,200,20],np.uint8)
+upColor = np.array([135,255,255],np.uint8)
+kernel = np.ones((5,5),np.uint8)
+
+def constant():
+    camera.capture('/home/pi/comp_vis/Assignment2/constant.jpg')
+    img=cv.imread('/home/pi/comp_vis/Assignment2/constant.jpg')
+    h,w=img.shape[:2]
+    ncm,roi=cv.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+    new = cv.undistort(img,mtx,dist,None,ncm)
+    x,y,w,h = roi
+    global wi
+    wi=w+1
+    new=new[y:y+h,x:x+w]
+    cv.imwrite('/home/pi/comp_vis/new.jpg',new)
+
 # I2C
 bus = smbus.SMBus(1)
 address = 0x04
@@ -38,12 +76,72 @@ def found(tape): # tape found
     else:
         return found
     
+state = none
+
+while(True):
+    try:
+        constant()
+        img2 = cv.imread('/home/pi/comp_vis/new.jpg')
+        #cv.imshow('pic', img2)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+        hsv = cv.cvtColor(img2,cv.COLOR_BGR2HSV)
+        #cv.imshow('hsv', hsv)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+        mask = cv.inRange(hsv,lowColor,upColor)
+        final = cv.bitwise_and(img2,img2, mask = mask)
+        #cv.imshow('Final', final)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+        kernel = np.ones((5,5),np.uint8)
+        clean = cv.blur(final,(3,3))
+        clean2 = cv.morphologyEx(clean, cv.MORPH_OPEN, kernel)
+        clean3 = cv.morphologyEx(clean2, cv.MORPH_CLOSE, kernel)
+        #cv.imshow('Clean', clean3)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+        grey = cv.cvtColor(clean3,cv.COLOR_BGR2GRAY)
+
+        #cv.imshow("contours", grey)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+    
+
+        ret,thresh = cv.threshold(grey,15,255,cv.THRESH_BINARY)
+        img0,contours,heirarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        #cv.imshow("contours", img0)
+        #cv.waitKey(0)
+        #cv.destroyAllWindows()
+        try:
+            cnt_c = contours[0]
+            M_c = cv.moments(cnt_c)
+            cxc = int(M_c['m10']/M_c['m00'])
+            cyc = int(M_c['m01']/M_c['m00'])
+            tape = True
+#            print(cxc)
+#            print(cyc)
+            
+            fov = 62.2
+            if(cxc<(wi/2)): 
+                angle = (fov/2)*(((wi/2)-cxc)/((wi/2)))
+                print("The camera needs to turn {:.2f} degrees to center the marker." .format(angle))
+            else:
+                angle = -(fov/2)*((cxc-(wi/2))/(wi/2))
+                print("The camera needs to turn {:.2f} degrees to center the marker." .format(angle))
+#            cv.imshow("contours", img0)
+#            cv.waitKey(0)
+#            cv.destroyAllWindows()
+            
+        except IndexError:
+            tape= False
+            print("No markers found.")
+    except KeyboardInterrupt:
+        pass
 
 # temp (taken from CV)
-angle = 0.0
-state = none
-tape = False
-distance = 0.0
+
+
 
 # shell testing lcd
 ##while(True):
