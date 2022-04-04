473cf820b747782763b61b1d230d5d6e3c515754
roylundeen <roylundeen@gmail.com>
2022-04-04 11:22:52 -0600

    adding Demo2 Comms

---
+++ arduino/Demo2Comms/Demo2Comms.ino
@@ -0,0 +1,79 @@
+// EENG350 Demo1 Functions
+
+#include <Wire.h>
+
+#define SLAVE_ADDRESS 0x04
+
+int number, state;
+String angleS, distanceS;
+double angle, distance;
+
+
+void setup() {
+  Serial.begin(115200); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+
+  Serial.println("Ready!");
+}
+
+void loop() {
+  delay(100);
+}
+
+
+// callback for received data
+void receiveData(int byteCount){
+
+  while(Wire.available()) {
+    int a = 0, s;
+    int address = Wire.read(); // address sent first
+
+    s = Wire.read() - 47; // read in state
+    while(a != 'a') a = Wire.read();
+    
+    distanceS = ""; // read in distance values until 'a'
+    while(a != 'b') {
+        a = Wire.read();
+        if (a > 0) distanceS += (char)a;
+      }
+    
+    angleS = ""; // read in angle values until 'b'
+    while(a != 'c') {
+      a = Wire.read();
+      if (a > 0) angleS += (char)a;
+    }
+    Serial.println(s);
+    Serial.println(distanceS);
+    Serial.println(angleS);
+    switch (s) {
+      case 0:
+        Serial.println("none");
+        break;
+      case 1:
+        Serial.println("straight");
+        distance = distanceS.toDouble();
+        Serial.print(distance);
+        break;
+      case 2:
+        Serial.println("rotate");
+        angle = angleS.toDouble();
+        Serial.print(angle);
+        break;
+      case 3:
+        Serial.println("find");
+        break;
+    }
+  }
+}
+
+
+// callback for sending data
+void sendData(){
+  Wire.write((uint8_t *) &angle, 4);
+
+}
---
+++ pi/Demo2CommsPy/Demo2Comms.py
@@ -0,0 +1,117 @@
+# EENG350 Demo1 Reference Functions
+
+import smbus
+import time
+import board
+import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+
+# I2C
+bus = smbus.SMBus(1)
+address = 0x04
+i2c = board.I2C()  # uses board.SCL and board.SDA
+
+# RPI - LCD
+# Modify this if you have a different sized Character LCD
+lcd_columns = 16
+lcd_rows = 2
+# Initialise the LCD class
+lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+# Set LCD color to whiteish
+lcd.color = [200, 25, 25]
+
+
+def writeNumber(s): # distance and angle
+    
+    block = str(s) + "a" + str(distance) + "b" + str(angle) + "c"
+    data = []
+    i = 0
+    print("\n" + block + "\n")
+    for ch in block:
+        data.append(ord(ch))
+        i = i + 1
+    bus.write_i2c_block_data(address, 0, data)
+    return -1
+
+def readNumber():
+    #number = bus.read_byte(address)
+    #number = bus.read_byte_data(address, 0)
+    data = bus.read_i2c_block_data(address, 0, 4) # reading back float from Arduino
+    return data
+
+def none(tape): # 0 (do nothing)
+    lcd.text_direction = lcd.LEFT_TO_RIGHT
+    lcd.message = "stop"
+    writeNumber(0)
+    if not stop:
+        if tape:
+            lcd.clear()
+            return rotate
+        else:
+            lcd.clear()
+            return find
+    else:
+        return none
+
+def straight(tape): # 1 (distance)
+    lcd.text_direction = lcd.LEFT_TO_RIGHT
+    lcd.message = "Moving\nDistance: " + str(distance)
+    writeNumber(1)
+    if angle < 1:
+        return straight
+    else:
+        lcd.clear()
+        return rotate
+
+def rotate(tape): # 2 (angle)
+    lcd.text_direction = lcd.LEFT_TO_RIGHT
+    lcd.message = "Rotating\nAngle: " + str(angle)
+    writeNumber(2)
+    if angle < 1:
+        lcd.clear()
+        return straight
+    else:
+        return rotate
+
+def find(tape): # 3 (no tape found)
+    lcd.text_direction = lcd.LEFT_TO_RIGHT
+    lcd.message = "Finding Tape..."
+    writeNumber(3)
+    if tape:
+        lcd.clear()
+        return rotate
+    else:
+        return find
+    
+
+# temp (taken from CV)
+angle = -3.0
+state = none
+tape = False
+stop = False
+distance = 1.0
+
+i = 0
+while(True):
+    time.sleep(1)
+    print("state: ", state) # none
+    state = state(tape)
+    print("newstate: ", state) # find
+    time.sleep(1)
+    tape = True
+    angle = input("Angle: ")
+    state = state(tape)
+    print("newstate: ", state) # rotate
+    time.sleep(1)
+    angle = 1.4
+    state = state(tape)
+    print("newstate: ", state) # rotate
+    angle = .2
+    state = state(tape)
+    print("newstate: ", state) # straight
+    time.sleep(3)
+    angle = float(input("Angle: "))
+    state = state(tape)
+    print("newstate: ", state) # none
+    
+##    distance = input("Distance: ")
+##    angle = input("Angle: ")
