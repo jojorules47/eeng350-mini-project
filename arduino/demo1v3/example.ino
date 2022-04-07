902b193ac5f7c0126949c8ca0cc61217b2980c25
roylundeen <roylundeen@gmail.com>
2022-04-07 13:11:23 -0600

    Updated comms and arduino code

---
+++ arduino/demo1v3/example.ino
@@ -0,0 +1,101 @@
+/**
+ * Example showing feedback control of robot, to acheive
+ * steady forward and rotational velocity using simple control
+ * loops.
+ */
+ 
+#include "controls.h"
+#define SLAVE_ADDRESS 0x04
+
+unsigned long time_now = 0;
+
+
+void setup() {
+
+  // pin setup
+  pinMode(enablePin, OUTPUT);
+  pinMode(directionA, OUTPUT);
+  pinMode(directionB, OUTPUT);
+  pinMode(speedA, OUTPUT);
+  pinMode(speedB, OUTPUT);
+  pinMode(statusFlag, INPUT);
+
+  digitalWrite(enablePin, HIGH);       // turn motor on
+  digitalWrite(directionA, rotationA); // set direction of motor
+  digitalWrite(directionB, rotationB); // set direction of motor
+  
+  Serial.begin(115200); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+//  Wire.onRequest(sendData);
+  Serial.println("Ready!");
+
+}
+
+bool first = false;
+// Millis delay variables
+double target_vel = 0.0;
+double target_turn = 0.0;
+int sleep = 1000;
+
+//double pos = 0.0;
+//double targetPos = 5.0;
+
+void loop() {
+  // static double motor_voltage = 0.0;
+
+  // Step motor position to PI/2 after 1 second
+  if (millis() >= sleep && first == false) {
+   // target_vel = 0.0;
+   // target_turn = PI/4;
+//         camera_state = get_next_state(camera_state);
+//      update_target(camera_state);
+//    camera_state = 1;
+//    camera_angle = PI/2;
+//    camera_distance=0.0;
+    get_next_state();
+    first = true;
+//    Serial.println("changing");
+  }
+
+
+
+/*  long encCountsA = encA.read();
+  long encCountsB = encB.read();
+ 
+  double currentPosA = ((double)encCountsA * 2.0 * PI) / 3200.0;
+  double currentPosB = ((double)encCountsB * 2.0 * PI) / 3200.0;
+  double 
+  */
+
+  // Read motor position, and determine motor voltage from PID controller
+  // This is encapsulated in a function in `motorTest.ino`
+  if (millis() >= time_now + SAMPLE_TIME) {
+    time_now += SAMPLE_TIME;
+
+    // Control motors to move in a straight line. See `motor_controls.ino`
+//    Serial.print(target_turn);
+//    Serial.print(", ");
+//    Serial.print(target_vel);
+//    Serial.print(" ");
+//Serial.print("State: ");
+//Serial.println(camera_state);
+    bool done_yet = motor_control(camera_state, camera_distance, camera_angle);
+          
+    if(done_yet){
+//          while(1);
+//          Serial.println("we done");
+//Serial.println("done");
+//      camera_state = get_next_state(camera_state);
+//      update_target(camera_state);
+      get_next_state();
+      reset_encoders();
+    }
+    
+    if (millis() > time_now + SAMPLE_TIME)
+      Serial.println("Took too long");
+  }
+}
--- pi/Demo2CommsPy/Demo2Comms.py
+++ pi/Demo2CommsPy/Demo2Comms.py
@@ -3,28 +3,33 @@
 import smbus
 import time
 import board
-import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
+##import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
 
 # I2C
 bus = smbus.SMBus(1)
 address = 0x04
 i2c = board.I2C()  # uses board.SCL and board.SDA
 
-# RPI - LCD
-# Modify this if you have a different sized Character LCD
-lcd_columns = 16
-lcd_rows = 2
-# Initialise the LCD class
-lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
-# Set LCD color to whiteish
-lcd.color = [200, 25, 25]
+### RPI - LCD
+### Modify this if you have a different sized Character LCD
+##lcd_columns = 16
+##lcd_rows = 2
+### Initialise the LCD class
+##lcd = character_lcd.Character_LCD_RGB_I2C(i2c, lcd_columns, lcd_rows)
+### Set LCD color to whiteish
+##lcd.color = [200, 25, 25]
 
 
-def writeNumber(s): # distance and angle
-    
-    block = str(s) + "a" + str(distance) + "b" + str(angle) + "c"
+def writeNumber(s, tape): # distance and angle
+    if tape:
+        t = 1
+    else:
+        t = 0
+        
+    block = str(s) + "a" + str(distance) + "b" + str(angle) + "c" + str(t)
     data = []
     i = 0
+    print(block)
     for ch in block:
         data.append(ord(ch))
         i = i + 1
@@ -37,80 +42,125 @@ def readNumber():
     data = bus.read_i2c_block_data(address, 0, 4) # reading back float from Arduino
     return data
 
+
 def none(tape): # 0 (do nothing)
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    lcd.message = "stop"
-    writeNumber(0)
+##    lcd.text_direction = lcd.LEFT_TO_RIGHT
+##    lcd.message = "stop"
     if not stop:
         if tape:
-            lcd.clear()
+##            lcd.clear()
+            writeNumber(2, tape)
             return rotate
         else:
-            lcd.clear()
+##            lcd.clear()
+            writeNumber(3, tape)
             return find
     else:
         return none
 
 def straight(tape): # 1 (distance)
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    lcd.message = "Moving\nDistance: " + str(distance)
-    writeNumber(1)
+##    lcd.text_direction = lcd.LEFT_TO_RIGHT
+##    lcd.message = "Moving\nDistance: " + str(distance)
     if angle < 1:
+        writeNumber(1, tape)
         return straight
     else:
-        lcd.clear()
-        return rotate
+##        lcd.clear()
+        return straight
 
 def rotate(tape): # 2 (angle)
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    lcd.message = "Rotating\nAngle: " + str(angle)
-    writeNumber(2)
+##    lcd.text_direction = lcd.LEFT_TO_RIGHT
+##    lcd.message = "Rotating\nAngle: " + str(angle)
     if float(angle) < 1:
-        lcd.clear()
+##        lcd.clear()
+        writeNumber(1, tape)
         return straight
     else:
+        #writeNumber(2, tape)
         return rotate
 
 def find(tape): # 3 (no tape found)
-    lcd.text_direction = lcd.LEFT_TO_RIGHT
-    lcd.message = "Finding Tape..."
-    
-    temp = int(input("Is tape found? (0/1): ")) ################### TESTING
-    if temp:
-        tape = True
-    else:
-        tape = False
-    
-    writeNumber(3)
-    if tape == True:
-        lcd.clear()
-        return rotate
+##    lcd.text_direction = lcd.LEFT_TO_RIGHT
+##    lcd.message = "Finding Tape..."
+
+
+    #print("len", da_input.len)
+    #temp = int(da_input.strip()) ################### TESTING
+##    if temp == 1:
+##        tape = True
+##    else:
+##        tape = False
+        
+    if tape:
+##        lcd.clear()
+        writeNumber(0, tape)
+        return none
     else:
         return find
-    
+
+##states = {'DO_NOTHING': 0, 'TURN': 2, 'GO_FORWARD': 1, 'FIND_TAPE': 3}
+##
+##class messenger(object):
+##    def __init__(self, distance, angle):
+##        self.distance = distance
+##        self.angle = angle
+##
+##        self.tape_found = False
+##        self.state = states['DO_NOTHING']
+##
+##    def stop(self):
+##    def turn(self):
+##    def drive(self):
+##    def find_tape(self):
+##
+##    def write_robot
+
+        
 
 # temp (taken from CV)
-angle = -3.0
+angle = 69.0
 state = none
 tape = False
 stop = False
-distance = 1.0
+distance = 42.0
 
 i = 0
-while(True):
-##    time.sleep(1)
-##    print("state: ", state) # none
-##    state = state(tape)
-##    print("newstate: ", state) # find
-##    time.sleep(1)
-##    tape = True
-##    angle = input("Angle: ")
-##    state = state(tape)
-##    print("newstate: ", state) # rotate
-##    time.sleep(1)
-##    angle = 1.4
-##    state = state(tape)
-##    print("newstate: ", state) # rotate
+if __name__ == '__main__':
+    print("Starting Robot")
+    time.sleep(1)
+    #print("state: ", state) # none
+    state = state(tape)
+    print("Finding tape")
+    #print("newstate: ", state) # find
+    time.sleep(1)
+    state = state(tape)
+    #print("newstate: ", state) # find
+    print("Finding tape")
+    time.sleep(1)
+    tape = True
+    print("Stopping Robot")
+    state = state(tape) # return none
+    #print("newstate: ", state) # none
+    
+    #time.sleep(1)
+    #state = state(tape)
+    #print("newstate: ", state)
+
+    
+    #time.sleep(1)
+    #print("Rotating Robot")
+    #angle = 1.4
+    #state = state(tape)
+
+    time.sleep(1)
+    print("Driving Robot")
+    angle = 2.0
+    distance = 4.0
+    writeNumber(1, tape)
+    writeNumber(2, tape)
+    time.sleep(1)
+    writeNumber(0, tape)
+    #print("newstate: ", state) # rotate
 ##    angle = .2
 ##    state = state(tape)
 ##    print("newstate: ", state) # straight
@@ -119,8 +169,10 @@ while(True):
 ##    state = state(tape)
 ##    print("newstate: ", state) # none
 
-    print("Current State: ", state)
-    distance = input("Distance: ")
-    angle = input("Angle: ")
-
-    state = state(tape)
+    #print("New State: ", state)
+##    distance = input("Distance: ")
+##    angle = input("Angle: ")
+##
+##    state = state(tape)
+##    print("Current State: ", state)
+##    
