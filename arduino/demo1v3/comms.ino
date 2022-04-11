c0e73082153f4a98186306431d926c72fe285171
Josiah Smith <jojorules47@gmail.com>
2022-04-10 19:49:11 -0600

    Re-implement comms and states

--- arduino/demo1v3/comms.ino
+++ arduino/demo1v3/comms.ino
@@ -5,156 +5,156 @@
 
 #define SLAVE_ADDRESS 0x04
 
-int pi_state=DO_NOTHING;
+int pi_state = DO_NOTHING;
 
-double pi_angle, pi_distance= 0.0;
+double pi_angle, pi_distance = 0.0;
 int camera_state = DO_NOTHING;
 double camera_angle = 0.0;
 double camera_distance = 0.0;
 bool ack = false;
-  
 
-//void setup() {
-//  Serial.begin(115200); // start serial for output
-//  // initialize i2c as slave
-//  Wire.begin(SLAVE_ADDRESS);
+// void setup() {
+//   Serial.begin(115200); // start serial for output
+//   // initialize i2c as slave
+//   Wire.begin(SLAVE_ADDRESS);
 //
-//  // define callbacks for i2c communication
-//  Wire.onReceive(receiveData);
-//  Wire.onRequest(sendData);
+//   // define callbacks for i2c communication
+//   Wire.onReceive(receiveData);
+//   Wire.onRequest(sendData);
 //
-//  Serial.println("Ready!");
-//}
+//   Serial.println("Ready!");
+// }
 
-//void loop() {
-//  delay(100);
-//}
-
+// void loop() {
+//   delay(100);
+// }
 
 // callback for received data
 bool dataReady = false;
-void receiveData(int byteCount){
+void receiveData(int byteCount) {
   String angleS, distanceS;
-  while(Wire.available()) {
+  while (Wire.available()) {
     int a = 0, s, t;
     int address = Wire.read(); // address sent first
 
     s = Wire.read() - '0'; // read in state
-    while(a != 'a') a = Wire.read();
-    
-    distanceS = ""; // read in distance values until 'a'
-    while(a != 'b') {
-        a = Wire.read();
-        if (a > 0) distanceS += (char)a;
-      }
-    
-    angleS = ""; // read in angle values until 'b'
-    while(a != 'c') {
+    while (a != 'a')
       a = Wire.read();
-      if (a > 0) angleS += (char)a;
+
+    distanceS = ""; // read in distance values until 'a'
+    while (a != 'b') {
+      a = Wire.read();
+      if (a > 0)
+        distanceS += (char)a;
+    }
+
+    angleS = ""; // read in angle values until 'b'
+    while (a != 'c') {
+      a = Wire.read();
+      if (a > 0)
+        angleS += (char)a;
     }
     t = Wire.read() - 48;
-    //Serial.println(t);
+    // Serial.println(t);
     pi_state = s;
     tape_found = t;
-    
+
     switch (s) {
-      case DO_NOTHING:
-        //Serial.println("none");
-        if(camera_state == FIND_TAPE) tape_found=true;
-        break;
-      case GO_FORWARD:
-        //Serial.println("straight");
-        pi_distance = distanceS.toDouble();
+    case DO_NOTHING:
+      // Serial.println("none");
+      if (camera_state == FIND_TAPE)
+        tape_found = true;
+      break;
+    case GO_FORWARD:
+      // Serial.println("straight");
+      pi_distance = distanceS.toDouble();
       //  Serial.print(pi_distance);
-        break;
-      case TURN:
-        //Serial.println("rotate");
-        pi_angle = angleS.toDouble();
-        //Serial.print(pi_angle);
-        break;
-      case FIND_TAPE:
-        //Serial.println("find");
-        pi_angle = angleS.toDouble();
-        break;
-      default:
-        pi_state = 0;
-        //Serial.print("Out of bounds");
+      break;
+    case TURN:
+      // Serial.println("rotate");
+      pi_angle = angleS.toDouble();
+      // Serial.print(pi_angle);
+      break;
+    case FIND_TAPE:
+      // Serial.println("find");
+      pi_angle = angleS.toDouble();
+      break;
+    default:
+      pi_state = 0;
+      // Serial.print("Out of bounds");
     }
   }
   dataReady = true;
 }
 
 // callback for sending data
-void sendData(){
+void sendData() {
   Serial.println("Sending ack...");
   Wire.write(1);
 }
 
+void get_next_state() {
+  //  int next_state = 0;
+  //  if(Serial.available() > 0){
+  //    next_state = Serial.read() - '0';
+  ////    Serial.print(input);
+  //  } else{
+  //    next_state = last_state;
+  //  }
+  //
+  //  if(next_state < 0 || next_state > 3){
+  //    Serial.print("State not recognized: ");
+  //    Serial.println(next_state);
+  //
+  //        next_state = 0;
+  //  }
 
-void get_next_state(){
-//  int next_state = 0;
-//  if(Serial.available() > 0){
-//    next_state = Serial.read() - '0';
-////    Serial.print(input);
-//  } else{
-//    next_state = last_state;
-//  }
-//
-//  if(next_state < 0 || next_state > 3){
-//    Serial.print("State not recognized: ");
-//    Serial.println(next_state);
-//
-//        next_state = 0;
-//  }
+  // static int last_state = 0;
+  //  if(camera_state != pi_state){
+  //
+  //  }
+  // bool updated = (camera_distance != pi_distance) || (camera_angle !=
+  // pi_angle);
 
-  //static int last_state = 0;
-//  if(camera_state != pi_state){
-//
-//  }
-  //bool updated = (camera_distance != pi_distance) || (camera_angle != pi_angle);
+  // pi_angle = 0.0;
+  // pi_distance = 0.0;
+  // pi_state = 2;
+  // dataReady = true;
 
-  pi_angle = 0.0;
-  pi_distance = 0.0;
-  pi_state = 2;
-  dataReady = true;
-  
-  if(dataReady){
+  if (dataReady) {
     camera_state = pi_state;
     camera_distance = pi_distance;
     camera_angle = pi_angle;
     sendData();
     dataReady = false;
-    
-    //Serial.print("Current State: ");
-    //Serial.print(camera_state);
+
+    // Serial.print("Current State: ");
+    // Serial.print(camera_state);
     Serial.print("New State: ");
     Serial.print(pi_state);
     Serial.print(" Distance: ");
     Serial.print(camera_distance);
     Serial.print(" Angle: ");
     Serial.println(camera_angle);
-  //last_state = camera_state;
+    // last_state = camera_state;
   }
-  
-//  return next_state;
 
+  //  return next_state;
 }
 
-//void send_ack(){
-//  Wire.beginTransmission()
-//  Wire.write(true)
-//  Wire.endTransmission()
-//}
+// void send_ack(){
+//   Wire.beginTransmission()
+//   Wire.write(true)
+//   Wire.endTransmission()
+// }
 
-void update_target(int state){
-  switch(state){
-    case TURN:
-      camera_angle = PI/2;
-      break;
-      case GO_FORWARD:
-      camera_distance = 3.0;
-      break;
+void update_target(int state) {
+  switch (state) {
+  case TURN:
+    camera_angle = PI / 2;
+    break;
+  case GO_FORWARD:
+    camera_distance = 3.0;
+    break;
   }
-
 }
--- arduino/demo1v3/demo1v3.ino
+++ arduino/demo1v3/demo1v3.ino
@@ -1,7 +1,7 @@
 /**
  * Functions used in the feedback control of the robot. Include
  * these files for a "drop in" solution for robot controls.
- * 
+ *
  * TODO: Make solution work
  */
 
@@ -12,10 +12,11 @@
 #define FIND_TAPE 3
 
 // Motor control utilities
-int volt_to_pwm(double volts) { 
+int volt_to_pwm(double volts) {
   double corr_volts = abs(volts);
-  corr_volts = corr_volts >= 5.0 ? 5.0: corr_volts;
-  return (int)(corr_volts / 5.0 * 255.0); }
+  corr_volts = corr_volts >= 5.0 ? 5.0 : corr_volts;
+  return (int)(corr_volts / 5.0 * 255.0);
+}
 bool volt_to_dir(double volts) { return (bool)(volts <= 0.0); }
 
 /**
@@ -25,7 +26,7 @@ bool volt_to_dir(double volts) { return (bool)(volts <= 0.0); }
 double read_motor(Encoder &enc, double &lastPos) {
   long newPosition = enc.read();
   double currentPos = ((double)newPosition * 2.0 * PI) / 3200.0;
-  //double currentPos = ((double)newPosition * 360.0) / 3200.0;
+  // double currentPos = ((double)newPosition * 360.0) / 3200.0;
   double velocity = (currentPos - lastPos) / (double)SAMPLE_TIME * 1000.0;
 
   lastPos = currentPos;
@@ -33,14 +34,14 @@ double read_motor(Encoder &enc, double &lastPos) {
   return velocity;
 }
 
-double read_angle(){
-  double radiansA = ((double)encA.read() * 2.0 * PI)/ 3200.0;
-  double radiansB = -1.0*((double)encB.read() * 2.0 * PI)/ 3200.0;
-  double phi = wheel_size*(radiansB - radiansA)/wheel_dist;
+double read_angle() {
+  double radiansA = ((double)encA.read() * 2.0 * PI) / 3200.0;
+  double radiansB = -1.0 * ((double)encB.read() * 2.0 * PI) / 3200.0;
+  double phi = wheel_size * (radiansB - radiansA) / wheel_dist;
   return phi;
 }
 
-void reset_encoders(){
+void reset_encoders() {
   encA.write(0);
   encB.write(0);
 }
@@ -50,35 +51,12 @@ void reset_encoders(){
  * https://www.teachmemicro.com/arduino-pid-control-tutorial/
  * control_t struct tracks error terms
  */
-double controller(double current, double target,
-                  struct control_t &pid) {
-  // TODO: Investigate using constant sample time instead of current time for simplicity
+double controller(double current, double target, struct control_t &pid) {
+  // TODO: Investigate using constant sample time instead of current time for
+  // simplicity
   unsigned long currentTime = millis(); // Get current time
-  double elapsedTime = (double)(currentTime - pid.previousTime) / 1000.0; // Calculate interval
-
-  pid.error = target - current;               // Determine current error
-  pid.total_error += pid.error * elapsedTime; // Calculate integrated error
-  double rate_error = (pid.error - pid.last_error) / elapsedTime; // Calculate derivative error
-
-  double out = pid.p * pid.error + pid.i * pid.total_error + pid.d * rate_error; // Total PID output
-
-  pid.last_error = pid.error;
-  pid.previousTime = currentTime;
-
-  // Limit motor voltage, and prevent wind-up
-  if (out > 5.0)
-    out = 5.0;
-  if (out < -5.0)
-    out = -5.0;
-
-  return out;
-}
-
-double controller(double current, double target,double limit,
-                  struct control_t &pid) {
-  // TODO: Investigate using constant sample time instead of current time for simplicity
-  unsigned long currentTime = millis(); // Get current time
-  double elapsedTime = (double)(currentTime - pid.previousTime) / 1000.0; // Calculate interval
+  double elapsedTime =
+      (double)(currentTime - pid.previousTime) / 1000.0; // Calculate interval
 
   pid.error = target - current;               // Determine current error
   pid.total_error += pid.error * elapsedTime; // Calculate integrated error
@@ -92,13 +70,41 @@ double controller(double current, double target,double limit,
   pid.previousTime = currentTime;
 
   // Limit motor voltage, and prevent wind-up
-  if (out > limit){
+  if (out > 5.0)
+    out = 5.0;
+  if (out < -5.0)
+    out = -5.0;
+
+  return out;
+}
+
+double controller(double current, double target, double limit,
+                  struct control_t &pid) {
+  // TODO: Investigate using constant sample time instead of current time for
+  // simplicity
+  unsigned long currentTime = millis(); // Get current time
+  double elapsedTime =
+      (double)(currentTime - pid.previousTime) / 1000.0; // Calculate interval
+
+  pid.error = target - current;               // Determine current error
+  pid.total_error += pid.error * elapsedTime; // Calculate integrated error
+  double rate_error =
+      (pid.error - pid.last_error) / elapsedTime; // Calculate derivative error
+
+  double out = pid.p * pid.error + pid.i * pid.total_error +
+               pid.d * rate_error; // Total PID output
+
+  pid.last_error = pid.error;
+  pid.previousTime = currentTime;
+
+  // Limit motor voltage, and prevent wind-up
+  if (out > limit) {
     out = limit;
     pid.total_error = limit;
   }
-  if (out < -1.0*limit){
-    out = -1.0*limit;
-    pid.total_error = -1.0*limit;
+  if (out < -1.0 * limit) {
+    out = -1.0 * limit;
+    pid.total_error = -1.0 * limit;
   }
 
   return out;
@@ -107,31 +113,32 @@ double controller(double current, double target,double limit,
 /**
  * Inner loop velocity controller to control forward velocity (rho_dot).
  */
-double motor_speed(double velA, double velB, double distance, bool& done) {
-//  double rho_dot = (velA + velB) / 2.0;
-static double lastX = 0.0;
-static double lastY = 0.0;
-//static double phiOld = 0.0;
+double motor_speed(double velA, double velB, double distance, bool &done) {
+  //  double rho_dot = (velA + velB) / 2.0;
+  static double lastX = 0.0;
+  static double lastY = 0.0;
+  // static double phiOld = 0.0;
   double rho_dot = wheel_size * (velA + velB) / 2.0;
 
   double target_vel = 0.5;
-  
 
+  // double voltage = controller(nextX, distance, 3.0, forwardPID);
+  double voltage = controller(rho_dot, target_vel, 3.0, forwardPID);
 
   double phi = read_angle();
-  double nextX = lastX + SAMPLE_TIME/1000.0 * wheel_size* cos(phi)* (velA+velB)/2.0;
-  double nextY = lastY + SAMPLE_TIME/1000.0 * wheel_size* sin(phi)* (velA+velB)/2.0;
-  
-  double voltage = controller(nextX, distance, 3.0, forwardPID);
+  double nextX = lastX + SAMPLE_TIME / 1000.0 * wheel_size * cos(phi) *
+                             (velA + velB) / 2.0;
+  double nextY = lastY + SAMPLE_TIME / 1000.0 * wheel_size * sin(phi) *
+                             (velA + velB) / 2.0;
 
-//  if(nextX >= distance*0.96){
-//    voltage = 0.0;
-//    done = true;
-//    lastX = lastY = 0.0;
-//    nextX = nextY = 0.0;
-//    forwardPID.total_error = 0.0;
-//    Serial.print("Target acheived");
-//  }
+  if (nextX >= distance * 0.96) {
+    voltage = 0.0;
+    done = true;
+    lastX = lastY = 0.0;
+    nextX = nextY = 0.0;
+    forwardPID.total_error = 0.0;
+    Serial.print("Target acheived");
+  }
 
   Serial.print(nextX);
   Serial.print("/");
@@ -140,7 +147,7 @@ static double lastY = 0.0;
   Serial.print(lastX);
   Serial.print(" ");
   Serial.println(voltage);
-//  }
+  //  }
   lastX = nextX;
   lastY = nextY;
 
@@ -150,59 +157,60 @@ static double lastY = 0.0;
 /**
  * Inner loop rotational velocity controller to control turning (phi_dot).
  */
-//double motor_direction(double velA, double velB, double turning) {
+// double motor_direction(double velA, double velB, double turning) {
 ////  double phi_dot = (velA - velB);
 //
 //  double current_angle = read_angle();
 //
 //  double target_phi = controller(current_angle, turning, turningPID);
-//  double my_phi_dot = wheel_size*(velA - velB)/wheel_dist; // wheel_size, wheel_dist
+//  double my_phi_dot = wheel_size*(velA - velB)/wheel_dist; // wheel_size,
+//  wheel_dist
 //
 //  return controller(my_phi_dot, target_phi, turningPID);
 //}
 
 bool angle_done = false;
-double motor_direction(double velA, double velB, double turning, bool& done) {
-//  double phi_dot = (velA - velB);
-//double my_angle = PI/2;
-  double current_angle = read_angle()/2;
+double motor_direction(double velA, double velB, double turning, bool &done) {
+  //  double phi_dot = (velA - velB);
+  // double my_angle = PI/2;
+  double current_angle = read_angle() / 2;
 
   double target_phi = controller(current_angle, turning, 0.2, anglePID);
-//  if(target_phi > 0.2) target_phi = 0.2;
+  //  if(target_phi > 0.2) target_phi = 0.2;
 
-  double phi_dot = wheel_size*(velA - velB)/wheel_dist; // wheel_size, wheel_dist
+  double phi_dot =
+      wheel_size * (velA - velB) / wheel_dist; // wheel_size, wheel_dist
 
-//  double voltage = controller(phi_dot, target_phi, 1.3, turningPID);
-  double voltage = controller(current_angle, turning, 1.75, turningPID);
+  double voltage = controller(phi_dot, target_phi, 1.3, turningPID);
+  // double voltage = controller(current_angle, turning, 1.75, turningPID);
 
   Serial.print(current_angle);
   Serial.print("/");
   Serial.print(turning);
   Serial.print(", ");
   Serial.println(voltage);
-//  if(turning != 0.0 && abs(current_angle) >= abs(turning)){
-//    voltage = 0.0;
-//    done = true;
-//  }
+  if (turning != 0.0 && abs(current_angle) >= abs(turning)) {
+    voltage = 0.0;
+    done = true;
+  }
 
-    return voltage;
-    
+  return voltage;
 }
 /**
  * Control motors to give robot specified forward speed and
  * rotational velocity using inner control loops. Place this function
  * in 'main' loop to control robot at a sampled time.
  */
- double targetPos = 2.0;
- double targetAngle = 0.0;
+double targetPos = 2.0;
+double targetAngle = 0.0;
 
-//typedef enum{
-//  DO_NOTHING,
-//  GO_FORWARD,
-//  TURN,
-//  FIND_TAPE
-//}robot_states ;
- 
+// typedef enum{
+//   DO_NOTHING,
+//   GO_FORWARD,
+//   TURN,
+//   FIND_TAPE
+// }robot_states ;
+
 bool motor_control(int &command, double target_distance, double target_angle) {
   static double lastX = 0.0;
   static double lastY = 0.0;
@@ -210,118 +218,120 @@ bool motor_control(int &command, double target_distance, double target_angle) {
   static double motorA_pos = 0.0;
   static double motorB_pos = 0.0;
 
-//static robot_states current_state = command;
+  // static robot_states current_state = command;
 
   bool done = false;
-  
+
   double targetX = target_distance * cos(target_angle);
   double targetY = target_distance * sin(target_angle);
 
   double velA = -1.0 * read_motor(encA, motorA_pos);
-  double velB = 1.0*read_motor(encB, motorB_pos);
+  double velB = 1.0 * read_motor(encB, motorB_pos);
 
   double forward_volts, turning_volts, fudge;
-  switch(command){
-    case DO_NOTHING:
-      forward_volts = 0;
-      turning_volts = 0;
-      motorA_pos = motorB_pos = 0.0;
-      done = true;
-      //Serial.println("DO_NOTHING");
+  switch (command) {
+  case DO_NOTHING:
+    forward_volts = 0;
+    turning_volts = 0;
+    motorA_pos = motorB_pos = 0.0;
+    done = true;
+    // Serial.println("DO_NOTHING");
     break;
-    case TURN:
-      forward_volts = 0.0;
-      turning_volts = motor_direction(velA, velB, target_angle, done);
-      fudge = 0*0.059;
-//      if(done || abs(target_angle) < 0.01){
-//        command = DO_NOTHING;
-//        
-//      }
-      //Serial.println("TURN");
+  case TURN:
+    forward_volts = 0.0;
+    turning_volts = motor_direction(velA, velB, target_angle, done);
+    fudge = 0 * 0.059;
+    if (done || abs(target_angle) < 0.01) {
+      command = DO_NOTHING;
+    }
+    // Serial.println("TURN");
+    break;
+  case GO_FORWARD:
+    forward_volts = motor_speed(velA, velB, target_distance, done);
+    turning_volts = 0.0;
+    fudge = 0 * 0.1;
+    // Serial.println("GO_FORWARD");
+    break;
+  case FIND_TAPE:
+    // TODO: implement
+    if (tape_found) {
+      command = DO_NOTHING;
       break;
-    case GO_FORWARD:
-        forward_volts = motor_speed(velA, velB, target_distance, done);
-        turning_volts = 0.0;
-        fudge = 0*0.1;
-      //Serial.println("GO_FORWARD");
-      break;
-    case FIND_TAPE:
-    //TODO: implement
-      if(tape_found){
-        command = DO_NOTHING;
-        break;}
-      forward_volts = 0.0;
-      turning_volts = motor_direction(velA, velB, target_angle, done);
-      //Serial.println("FIND_TAPE");
-      break;
-     
-    default:
-    
+    }
+    forward_volts = 0.0;
+    turning_volts = motor_direction(velA, velB, target_angle, done);
+    // Serial.println("FIND_TAPE");
+    break;
+
+  default:
+
     Serial.println("ERROR");
   }
-  if(done) command = DO_NOTHING;
+  if (done)
+    command = DO_NOTHING;
 
-  
-//  if(speed != 0.0){
-//    if(reset==1){
-//       encA.write(0);
-//       encB.write(0);
-//       reset = 0;
-//    }
-//    forward_volts = 0.75*motor_speed(velA, velB, speed);
-//    turning_volts = 0.0;
-//    fudge = 0.08;
-//  } else{
-//    forward_volts = 0.0;
-//    turning_volts = motor_direction(velA, velB, turning);
-//    fudge = -0.059;
-//  }
-//    if(angle_done == true){
-//      done = true;
-////      Serial.println("we done");
-//    }
+  //  if(speed != 0.0){
+  //    if(reset==1){
+  //       encA.write(0);
+  //       encB.write(0);
+  //       reset = 0;
+  //    }
+  //    forward_volts = 0.75*motor_speed(velA, velB, speed);
+  //    turning_volts = 0.0;
+  //    fudge = 0.08;
+  //  } else{
+  //    forward_volts = 0.0;
+  //    turning_volts = motor_direction(velA, velB, turning);
+  //    fudge = -0.059;
+  //  }
+  //    if(angle_done == true){
+  //      done = true;
+  ////      Serial.println("we done");
+  //    }
   double voltsA = (forward_volts + turning_volts) / 2.0;
   double voltsB = (forward_volts - turning_volts) / 2.0;
- // double nextX = lastX + SAMPLE_TIME/1000.0 * wheel_size* cos(phiOld)* (velA+velB)/2.0;
- // double nextY = lastY + SAMPLE_TIME/1000.0 * wheel_size* sin(phiOld)* (velA+velB)/2.0;
-  //double current_angle = read_angle();
-//  //double newPhi = phiOld + SAMPLE_TIME/1000.0 * (wheel_size/wheel_dist)*(velA-velB);
-//  //double newPhi = readAngle();
-//  if(speed != 0.0){
-//  if((nextX >= targetX*0.96)&&(nextY >= targetY*0.96)){
-//    voltsA = voltsB = 0.0;
-//    done = true;
-//  }
-//  Serial.print(nextX);
-//  Serial.print("/");
-//  Serial.print(targetX);
-//  Serial.print(" ");
-//  Serial.println(forwardPID.total_error);
-//  }
-//  lastX = nextX;
-//  lastY = nextY;
-  
-//  phiOld = phiNew;
+  // double nextX = lastX + SAMPLE_TIME/1000.0 * wheel_size* cos(phiOld)*
+  // (velA+velB)/2.0; double nextY = lastY + SAMPLE_TIME/1000.0 * wheel_size*
+  // sin(phiOld)* (velA+velB)/2.0;
+  // double current_angle = read_angle();
+  //  //double newPhi = phiOld + SAMPLE_TIME/1000.0 *
+  //  (wheel_size/wheel_dist)*(velA-velB);
+  //  //double newPhi = readAngle();
+  //  if(speed != 0.0){
+  //  if((nextX >= targetX*0.96)&&(nextY >= targetY*0.96)){
+  //    voltsA = voltsB = 0.0;
+  //    done = true;
+  //  }
+  //  Serial.print(nextX);
+  //  Serial.print("/");
+  //  Serial.print(targetX);
+  //  Serial.print(" ");
+  //  Serial.println(forwardPID.total_error);
+  //  }
+  //  lastX = nextX;
+  //  lastY = nextY;
 
-//  Serial.print(voltsA);
-//  Serial.print(", ");
-//  Serial.println(voltsB);
-//  Serial.print(lastX);
-//  Serial.print(", ");
-//  Serial.print(targetPos);
-//  Serial.print(", ");
-//   Serial.print(wheel_size*(velA+velB)/2.0,4);
-//  Serial.print(", ");
-//  Serial.print(wheel_size*(velA-velB)/wheel_dist,4);
-//  Serial.print(" , ");
-//  Serial.print(read_angle());
-//  Serial.print(" , ");
-//  Serial.println(targetAngle);
+  //  phiOld = phiNew;
 
-  analogWrite(speedA, volt_to_pwm(voltsA+fudge));
+  //  Serial.print(voltsA);
+  //  Serial.print(", ");
+  //  Serial.println(voltsB);
+  //  Serial.print(lastX);
+  //  Serial.print(", ");
+  //  Serial.print(targetPos);
+  //  Serial.print(", ");
+  //   Serial.print(wheel_size*(velA+velB)/2.0,4);
+  //  Serial.print(", ");
+  //  Serial.print(wheel_size*(velA-velB)/wheel_dist,4);
+  //  Serial.print(" , ");
+  //  Serial.print(read_angle());
+  //  Serial.print(" , ");
+  //  Serial.println(targetAngle);
+
+  analogWrite(speedA, volt_to_pwm(voltsA + fudge));
   digitalWrite(directionA, volt_to_dir(voltsA));
 
-  analogWrite(speedB, volt_to_pwm(voltsB-fudge));
+  analogWrite(speedB, volt_to_pwm(voltsB - fudge));
   digitalWrite(directionB, volt_to_dir(voltsB));
 
   return done;
